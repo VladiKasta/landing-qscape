63a56a24f143961c3f141b2b2b2361adeffe184a
vaseckk <nastavasina3682@gmail.com>
2025-09-11 16:20:32 +0300

    fix: cookies-modal-js

--- src/js/cookies-modal.js
+++ src/js/cookies-modal.js
@@ -22,3 +22,27 @@
         }
     }, 10000)
 })*/
+
+const modal = document.getElementById('cookiesModal');
+const acceptDtn = document.getElementById('cookiesAccept');
+const closeBtn = document.getElementById('cookiesClose');
+
+function checkCookies() {
+    if (document.cookie.includes('cookiesAccepted=')) {
+        modal.classList.remove('show');
+    } else {
+        modal.classList.add('show');
+    }
+}
+
+acceptDtn.addEventListener('click', function() {
+    document.cookie = "cookiesAccepted=true; max-age=2592000; path=/";
+    modal.classList.remove('show');
+});
+
+closeBtn.addEventListener('click', function() {
+    document.cookie = "cookiesAccepted=false; max-age=2592000; path=/";
+    modal.classList.remove('show');
+});
+
+checkCookies();
